ffd73ddbffa5d8e00efaabc81a64352f6fbc135f
Rostyslav <124372762+hockag@users.noreply.github.com>
2024-07-06 15:55:37 +0300

    add html and scss code also worked js code

--- src/javascript/leap-year.js
+++ src/javascript/leap-year.js
@@ -1,21 +1,24 @@
 function isLeapYear(year) {
-  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){
-    return true;
-  } else {
-    return false;
-  }
+  return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+  const input = document.getElementById('input');
+  const output = document.getElementById('output');
+  const checkButton = document.getElementById('check');
 
-function checkLeapYear() {
-    const input = document.getElementById('input');
-    const output = document.getElementById('output');
-    const output2 = document.getElementById('output2');
-    const check = document.getElementById("check");
+  checkButton.addEventListener('click', function() {
+    const yearString = input.value.trim(); // Отримуємо введене значення року як рядок
+    const year = parseInt(yearString, 10); // Перетворюємо рядок на ціле число
 
-
-  check.addEventListener("click", function() {
-    const year = parseInt(input.value, 10);
-
-    if (!isNaN(year)) {
-      if (isLeapYear(year))
+    if (!isNaN(year)) { // Перевіряємо, чи введений рік є числом
+      if (isLeapYear(year)) {
+        output.textContent = `Ви народилися у високосний рік!`; // Виводимо текст, якщо рік високосний
+      } else {
+        output.textContent = `Ви народилися не у високосний рік!`; // Виводимо текст, якщо рік не високосний
+      }
+    } else {
+      output.textContent = `Будь ласка, введіть коректний рік!`; // Виводимо текст, якщо введений рік не є числом
+    }
+  });
+});
