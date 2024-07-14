c7b0f03cc89af907c637b7ad9b16309c2a165e6b
Mykhailo <mykhailokhomiak@gmail.com>
2024-07-14 20:22:42 +0300

    Вирішення конфліктів після pull

--- src/JS/three_number.js
+++ src/JS/three_number.js
@@ -1,4 +1,3 @@
-console.log(1);
 const num1 = document.getElementById('number1');
 const num2 = document.getElementById('number2');
 const num3 = document.getElementById('number3');
@@ -19,6 +18,7 @@ function checkInputs() {
         } */
 
 }
-num1.addEventLisener('input', checkInputs)
-num2.addEventLisener('input', checkInputs)
-num3.addEventLisener('input', checkInputs)
+num1.addEventListener('input', checkInputs);
+num2.addEventListener('input', checkInputs);
+num3.addEventListener('input', checkInputs);
+
