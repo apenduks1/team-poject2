fbb53d82c34a1def33e14f092c6fdf1aff2a5cce
Vyacheslav <0907vk@gmail.com>
2024-07-29 20:58:07 +0300

    find: final

--- src/js/switch.js
+++ src/js/switch.js
@@ -1,17 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
   const switchElement = document.querySelector('.switch');
-  
+  const body = document.body;
+  const logoStroke = document.querySelectorAll('.logo-stroke');
+  const logoFill = document.querySelectorAll('.logo-fill');
+
   switchElement.addEventListener('click', function () {
     const isOn = switchElement.classList.toggle('on');
-    
+
     if (isOn) {
-      document.body.classList.remove('light-mode');
-      document.body.classList.add('dark-mode');
+      body.classList.remove('light-mode');
+      body.classList.add('dark-mode');
     } else {
-      document.body.classList.remove('dark-mode');
-      document.body.classList.add('light-mode');
+      body.classList.remove('dark-mode');
+      body.classList.add('light-mode');
     }
+
+    logoStroke.forEach(stroke => {
+      stroke.setAttribute('stroke', isOn ? 'white' : 'black');
+    });
+
+    logoFill.forEach(fill => {
+      fill.setAttribute('fill', isOn ? 'white' : 'black');
+    });
   });
-  
-  document.body.classList.add('light-mode');
-});
+
+  body.classList.add('light-mode');
+}); 
